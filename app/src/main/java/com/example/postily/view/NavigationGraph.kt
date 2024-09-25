b25a23cda0ee8ac4265f36a6394bc72c0cd0cb3d
Muhammad Ega <mes295@student.bham.ac.uk>
2024-09-25 16:41:27 +0100

    nav graph

---
+++ app/src/main/java/com/example/postily/view/NavigationGraph.kt
@@ -0,0 +1,61 @@
+package com.example.postily.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.postily.view.feed.FeedScreen
+import com.example.postily.view.albums.AlbumsScreen
+import com.example.postily.view.albums.AlbumDetailScreen
+import com.example.postily.view.feed.FeedDetailScreen
+import com.example.postily.view.profile.ProfileScreen
+import com.example.postily.view.profile.friend.FriendDetailScreen
+import com.example.postily.view.albums.photo.PhotoDetailScreen
+import com.example.postily.view.tasks.TaskScreen
+
+@Composable
+fun NavigationGraph(
+    navController: NavHostController,
+    paddingValues: PaddingValues // Accept padding values from the Scaffold
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "feed",
+        modifier = Modifier.padding(paddingValues) // Apply padding to the navigation content
+    ) {
+        // Feed Screen
+        composable("feed") { FeedScreen(navController) }
+
+        // Detailed Feed Screen for individual posts
+        composable("feedDetail/{postId}") { backStackEntry ->
+            FeedDetailScreen(navController, backStackEntry.arguments?.getString("postId"))
+        }
+
+        // Albums Screen
+        composable("albums") { AlbumsScreen(navController) }
+
+        // Detailed Album Screen for individual albums
+        composable("albumDetail/{albumId}") { backStackEntry ->
+            AlbumDetailScreen(navController, backStackEntry.arguments?.getString("albumId"))
+        }
+
+        // Photo detail screen
+        composable("photoDetail/{photoId}") { backStackEntry ->
+            PhotoDetailScreen(navController, backStackEntry.arguments?.getString("photoId"))
+        }
+
+        // Tasks Screen
+        composable("tasks") { TaskScreen(navController) }
+
+        // Profile (Me) Screen
+        composable("me") { ProfileScreen(navController) }
+
+        // Friend Details
+        composable("friendDetail/{friendId}") { backStackEntry ->
+            FriendDetailScreen(navController, backStackEntry.arguments?.getString("friendId"))
+        }
+    }
+}
