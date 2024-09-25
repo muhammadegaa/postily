3a09186bcd388a7f71400bf5a5673a60454ca0e1
Muhammad Ega <mes295@student.bham.ac.uk>
2024-09-25 16:34:43 +0100

    task view

--- app/src/main/java/com/example/postily/view/tasks/TaskScreen.kt
+++ app/src/main/java/com/example/postily/view/tasks/TaskScreen.kt
@@ -1,4 +1,36 @@
 package com.example.postily.view.tasks
 
-class TaskScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.postily.model.tasks.Task
+import com.example.postily.viewmodel.TaskViewModel
+
+@Composable
+fun TaskScreen(navController: NavController, viewModel: TaskViewModel = viewModel()) {
+    val tasks = viewModel.tasks
+
+    Column {
+        Text(text = "Ongoing Tasks")
+        LazyColumn {
+            items(tasks.filter { !it.completed }) { task ->
+                TaskItem(task)
+            }
+        }
+        Text(text = "Completed Tasks")
+        LazyColumn {
+            items(tasks.filter { it.completed }) { task ->
+                TaskItem(task)
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskItem(task: Task) {
+    Text(text = task.title)
 }
