dca358a60d027e5bd846b5fdfb95377eea743a2c
Muhammad Ega <mes295@student.bham.ac.uk>
2024-09-25 16:36:30 +0100

    user view model

---
+++ app/src/main/java/com/example/postily/viewmodel/UserViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.postily.viewmodel
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.postily.model.profile.User
+import com.example.postily.repository.UserRepository
+import kotlinx.coroutines.launch
+
+class UserViewModel : ViewModel() {
+    private val repository = UserRepository()
+
+    var user = mutableStateOf<User?>(null)
+        private set
+
+    var friends = mutableStateListOf<User>()
+        private set
+
+    init {
+        fetchUser()
+        fetchFriends()
+    }
+
+    // Fetch user profile
+    private fun fetchUser() {
+        viewModelScope.launch {
+            val fetchedUser = repository.getUser()
+            user.value = fetchedUser
+        }
+    }
+
+    // Fetch user's friends
+    private fun fetchFriends() {
+        v
