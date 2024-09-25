620a3e35c486c82979b5a7c7d43e6b2f2872669d
Muhammad Ega <mes295@student.bham.ac.uk>
2024-09-25 16:36:46 +0100

    feed view model

--- app/src/main/java/com/example/postily/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/example/postily/viewmodel/FeedViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.postily.viewmodel
 
-class FeedViewModel {
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.postily.model.feed.Comment
+import com.example.postily.model.feed.Post
+import com.example.postily.repository.FeedRepository
+import kotlinx.coroutines.launch
+
+class FeedViewModel : ViewModel() {
+    private val repository = FeedRepository()
+
+    var posts = mutableStateListOf<Post>()
+        private set
+
+    var comments = mutableStateListOf<Comment>()
+        private set
+
+    init {
+        fetchPosts()
+    }
+
+    private fun fetchPosts() {
+        viewModelScope.launch {
+            val fetchedPosts = repository.getPosts()
+            posts.addAll(fetchedPosts)
+        }
+    }
+
+    fun fetchComments(postId: Int) {
+        viewModelScope.launch {
+            val fetchedComments = repository.getComments(postId)
+            comments.clear()
+            comments.addAll(fetchedComments)
+        }
+    }
+
+    fun getPostById(postId: Int): Post? {
+        return posts.find { it.id == postId }
+    }
 }
