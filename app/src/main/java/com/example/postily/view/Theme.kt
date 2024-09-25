641196e814797fd5803dc71c4b0d59e48772a16e
Muhammad Ega <mes295@student.bham.ac.uk>
2024-09-25 16:42:05 +0100

    theme

---
+++ app/src/main/java/com/example/postily/view/Theme.kt
@@ -0,0 +1,32 @@
+package com.example.postily.view
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple200,
+    secondary = Teal200,
+    tertiary = Purple700
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple500,
+    secondary = Teal200,
+    tertiary = Purple700
+
+    // Add other colors if necessary
+)
+
+@Composable
+fun PostilyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+ 
